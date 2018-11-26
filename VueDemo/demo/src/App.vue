70fca2b66f6486f39c7d3594dd87b4615e9fc9b8
sjh <1158388143@qq.com>
2018-11-26 23:03:28 +0800

    vue学习

---
+++ VueDemo/demo/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <img class="logo" src="./assets/logo.png" alt="log"/>
+    <!--3.使用标签-->
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+  //1.引入组件
+  import HelloWorld from './components/HelloWorld'
+
+  export default {
+    //2.映射组件标签
+    components: {
+      HelloWorld
+    }
+  }
+</script>
+
+<style>
+  .logo{
+    width: 200px;
+    height: 200px;
+  }
+</style>
